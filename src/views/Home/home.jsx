f2dc11ece53460ad99cbc72c07ff6904d0acc2d4
xinghunm <xinghunm@curacloudcorp.com>
2019-04-22 13:24:48 +0800

    add home view

--- src/views/Home/home.jsx
+++ src/views/Home/home.jsx
@@ -1,5 +1,109 @@
 import React from 'react';
+import Table from 'src/components/Table';
 
-const Home = () => <div>welcome home</div>;
+const columnsData = [
+  {
+    name: '#',
+    dataIndex: 'questionNum',
+    width: '25%'
+  },
+  {
+    name: '题名',
+    dataIndex: 'questionTtile',
+    render: () => <div>123</div>
+  },
+  {
+    name: '练习次数',
+    dataIndex: 'exerciseTimes',
+    width: '20%',
+    render: () => <div>123</div>
+  },
+  {
+    name: '通过率',
+    dataIndex: 'passRate',
+    width: '15%',
+    render: () => <div>123</div>
+  },
+  {
+    name: '难度',
+    dataIndex: 'difficulty',
+    width: '15%',
+    render: () => <div>123</div>
+  }
+];
+
+const dataSource = [
+  {
+    questionNum: 120,
+    questionTtile: 'asdfsa',
+    exerciseTimes: 5,
+    passRate: '20%',
+    difficulty: 1
+  },
+  {
+    questionNum: 121,
+    questionTtile: 'asdfsa',
+    exerciseTimes: 5,
+    passRate: '20%',
+    difficulty: 1
+  },
+  {
+    questionNum: 122,
+    questionTtile: 'asdfsa',
+    exerciseTimes: 5,
+    passRate: '20%',
+    difficulty: 1
+  },
+  {
+    questionNum: 123,
+    questionTtile: 'asdfsa',
+    exerciseTimes: 5,
+    passRate: '20%',
+    difficulty: 1
+  },
+  {
+    questionNum: 124,
+    questionTtile: 'asdfsa',
+    exerciseTimes: 5,
+    passRate: '20%',
+    difficulty: 1
+  },
+  {
+    questionNum: 125,
+    questionTtile: 'asdfsa',
+    exerciseTimes: 5,
+    passRate: '20%',
+    difficulty: 1
+  },
+  {
+    questionNum: 126,
+    questionTtile: 'asdfsa',
+    exerciseTimes: 5,
+    passRate: '20%',
+    difficulty: 1
+  },
+  {
+    questionNum: 123,
+    questionTtile: 'asdfsa',
+    exerciseTimes: 5,
+    passRate: '20%',
+    difficulty: 1
+  },
+  {
+    questionNum: 127,
+    questionTtile: 'asdfsa',
+    exerciseTimes: 5,
+    passRate: '20%',
+    difficulty: 1
+  }
+];
+
+const Home = () => {
+  return (
+    <div>
+      <Table columns={columnsData} dataSource={dataSource} />
+    </div>
+  );
+};
 
 export default Home;
