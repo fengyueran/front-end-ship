db39805cf45a51af26499a8331d50b35b0509647
xinghunm <xinghunm@curacloudcorp.com>
2019-05-08 19:11:24 +0800

    add NavBar to home

--- src/views/Home/home.jsx
+++ src/views/Home/home.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Table from 'src/components/Table';
+import { VerticalBox } from '@xinghunm/widgets';
+import NavBar from '../NavBar';
 
 const columnsData = [
   {
@@ -95,14 +97,155 @@ const dataSource = [
     exerciseTimes: 5,
     passRate: '20%',
     difficulty: 1
+  },
+  {
+    questionNum: 120,
+    questionTtile: 'asdfsa',
+    exerciseTimes: 5,
+    passRate: '20%',
+    difficulty: 1
+  },
+  {
+    questionNum: 121,
+    questionTtile: 'asdfsa',
+    exerciseTimes: 5,
+    passRate: '20%',
+    difficulty: 1
+  },
+  {
+    questionNum: 122,
+    questionTtile: 'asdfsa',
+    exerciseTimes: 5,
+    passRate: '20%',
+    difficulty: 1
+  },
+  {
+    questionNum: 123,
+    questionTtile: 'asdfsa',
+    exerciseTimes: 5,
+    passRate: '20%',
+    difficulty: 1
+  },
+  {
+    questionNum: 124,
+    questionTtile: 'asdfsa',
+    exerciseTimes: 5,
+    passRate: '20%',
+    difficulty: 1
+  },
+  {
+    questionNum: 125,
+    questionTtile: 'asdfsa',
+    exerciseTimes: 5,
+    passRate: '20%',
+    difficulty: 1
+  },
+  {
+    questionNum: 126,
+    questionTtile: 'asdfsa',
+    exerciseTimes: 5,
+    passRate: '20%',
+    difficulty: 1
+  },
+  {
+    questionNum: 123,
+    questionTtile: 'asdfsa',
+    exerciseTimes: 5,
+    passRate: '20%',
+    difficulty: 1
+  },
+  {
+    questionNum: 127,
+    questionTtile: 'asdfsa',
+    exerciseTimes: 5,
+    passRate: '20%',
+    difficulty: 1
+  },
+  {
+    questionNum: 120,
+    questionTtile: 'asdfsa',
+    exerciseTimes: 5,
+    passRate: '20%',
+    difficulty: 1
+  },
+  {
+    questionNum: 121,
+    questionTtile: 'asdfsa',
+    exerciseTimes: 5,
+    passRate: '20%',
+    difficulty: 1
+  },
+  {
+    questionNum: 122,
+    questionTtile: 'asdfsa',
+    exerciseTimes: 5,
+    passRate: '20%',
+    difficulty: 1
+  },
+  {
+    questionNum: 123,
+    questionTtile: 'asdfsa',
+    exerciseTimes: 5,
+    passRate: '20%',
+    difficulty: 1
+  },
+  {
+    questionNum: 124,
+    questionTtile: 'asdfsa',
+    exerciseTimes: 5,
+    passRate: '20%',
+    difficulty: 1
+  },
+  {
+    questionNum: 125,
+    questionTtile: 'asdfsa',
+    exerciseTimes: 5,
+    passRate: '20%',
+    difficulty: 1
+  },
+  {
+    questionNum: 126,
+    questionTtile: 'asdfsa',
+    exerciseTimes: 5,
+    passRate: '20%',
+    difficulty: 1
+  },
+  {
+    questionNum: 123,
+    questionTtile: 'asdfsa',
+    exerciseTimes: 5,
+    passRate: '20%',
+    difficulty: 1
+  },
+  {
+    questionNum: 127,
+    questionTtile: 'asdfsa',
+    exerciseTimes: 5,
+    passRate: '20%',
+    difficulty: 1
+  },
+  {
+    questionNum: 123,
+    questionTtile: 'asdfsa',
+    exerciseTimes: 5,
+    passRate: '20%',
+    difficulty: 1
+  },
+  {
+    questionNum: 127,
+    questionTtile: 'asdfsa',
+    exerciseTimes: 5,
+    passRate: '20%',
+    difficulty: 1
   }
 ];
 
 const Home = () => {
   return (
-    <div>
+    <VerticalBox>
+      <NavBar />
       <Table columns={columnsData} dataSource={dataSource} />
-    </div>
+    </VerticalBox>
   );
 };
 
