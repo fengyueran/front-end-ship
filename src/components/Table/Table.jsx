1ced1c9772373d15cd435973973179dd42936d9d
xinghunm <xinghunm@curacloudcorp.com>
2019-05-08 19:38:09 +0800

    update @xinghunm/widgets and @xinghunm/eslint-config

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import Pagination from '../Pagination';
+import { Pagination } from '@xinghunm/widgets';
 
 const Container = styled.div``;
 
