536573771635248fff8bbf98810783dfa9b086fa
xinghunm <xinghunm@curacloudcorp.com>
2019-05-22 11:19:56 +0800

    add component ButtonBase

---
+++ src/components/ButtonBase/ButtonBase.jsx
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+import { Button, utils } from '@xinghunm/widgets';
+
+const ButtonBase = styled(Button).attrs(({ isDisable }) => ({
+  hasRipple: !isDisable
+}))`
+  font-size: 13px;
+  color: ${props => (props.isDisable ? '#afaeae' : '#37464e')};
+  border: solid 1px;
+  border-color: ${props => (props.isDisable ? '#E8E9E9' : '#c9d6da')};
+  outline: none;
+  padding: 6px 14px;
+  border-radius: 3px;
+  margin: 0;
+  min-height: 34px;
+  background: ${props => props.background || 'none'};
+
+  :hover {
+    background: ${props =>
+      props.isDisable ? 'none' : utils.fade('#337ab7', 0.2)};
+    cursor: ${props => (props.isDisable ? 'not-allowed' : 'pointer')};
+  }
+  box-shadow: none;
+`;
+
+export default ButtonBase;
