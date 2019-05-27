d86ad6d5db726de43109bd49023b7f509712bea0
xinghunm <xinghunm@curacloudcorp.com>
2019-05-27 14:08:00 +0800

    enable to show the ellipsis when the title is too long

--- src/views/QuestionTable/stateless/TitleCell.jsx
+++ src/views/QuestionTable/stateless/TitleCell.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { ROUTES } from 'src/utils/constants';
+
+const Container = styled.div`
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
+  max-width: 400px;
+`;
 
 const propTypes = {
   rowData: PropTypes.object,
@@ -13,9 +21,11 @@ const TitleCell = ({ rowData, updateQuestionParam }) => {
     updateQuestionParam({ currentQuestion: rowData });
   };
   return (
-    <Link onClick={handleClick} to={`${ROUTES.ANSWER}/${id}`}>
-      {title}
-    </Link>
+    <Container title={title}>
+      <Link onClick={handleClick} to={`${ROUTES.ANSWER}/${id}`}>
+        {title}
+      </Link>
+    </Container>
   );
 };
 
