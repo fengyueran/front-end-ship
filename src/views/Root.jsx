dc931d2962b356402baeb5129f28de4e08bccb39
xinghunm <xinghunm@curacloudcorp.com>
2019-12-06 19:30:13 +0800

    replace router with browser router

--- src/views/Root.jsx
+++ src/views/Root.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { Route, Switch, Router } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import { createHashHistory } from 'history';
 
 import { ROUTES } from 'src/utils/constants';
 import PrimaryLayout from './PrimaryLayout';
 
-const history = createBrowserHistory();
+const history = createHashHistory();
 
 const App = () => (
-  <Router history={history}>
+  <BrowserRouter history={history}>
     <Switch>
       <Route path={ROUTES.ROOT} component={PrimaryLayout} />
     </Switch>
-  </Router>
+  </BrowserRouter>
 );
 
 export default App;
